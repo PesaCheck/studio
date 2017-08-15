24a3d8226ffbb0c0715d2c35a59738ecfa3dfd73
Brian Wachanga <brian@talicraft.com>
2017-08-15 12:13:08 +0300

    list and forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,33 +9,27 @@ import { SocialStoryComponent } from "./social-story/social-story.component";
 import { StudioComponent } from "./studio/studio.component";
 
 const routes: Routes = [
-  
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'studio',
-    component: StudioComponent,
-    children: [
-      {
+
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
         path: 'stories',
         component: SocialStoriesComponent
-      },
-      {
+    },
+    {
         path: 'story',
         component: SocialStoryComponent
-      }
-    ]
-  },
-  {
-    path: '',
-    component: NotFoundComponent
-  }
+    },
+    {
+        path: '',
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
